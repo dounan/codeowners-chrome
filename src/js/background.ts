3d30850f74fc3af4ac9b639c5e2d1a2258fe63cc
Dounan Shi <dounan@gmail.com>
2018-08-14 00:01:17 -0700

    Use event filters instead of custom url parsing logic

--- src/js/background.ts
+++ src/js/background.ts
@@ -1,18 +1,11 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  if (isPrFilesPage(details.url)) {
+chrome.webNavigation.onHistoryStateUpdated.addListener(
+  function(details) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       const tab = tabs[0];
       tab != null &&
         tab.id != null &&
         chrome.tabs.sendMessage(tab.id, {type: "injectButton"});
     });
-  }
-});
-
-function isPrFilesPage(url: string) {
-  return (
-    url &&
-    url.indexOf("github.com") > 0 &&
-    url.replace(/\?.*/i, "").endsWith("/files")
-  );
-}
+  },
+  {url: [{hostSuffix: "github.com", pathSuffix: "/files"}]},
+);
